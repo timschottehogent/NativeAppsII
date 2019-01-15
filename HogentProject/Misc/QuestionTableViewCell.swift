3903cd1359a6973411b980a45880b12c5269512e
tim schotte <tim.schotte@delaware.pro>
2019-01-15 18:41:58 +0100

    (c) mention, see previous commit

--- HogentProject/Misc/QuestionTableViewCell.swift
+++ HogentProject/Misc/QuestionTableViewCell.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Tim Schotte on 29/12/2018.
 //  Copyright © 2018 Tim Schotte. All rights reserved.
+//  (c) I extended some exercises from the book "Everyone can code - App developement with swift" downloaded from the ibooks store
+//  It's possible there are similarities
+//  This class doesn't to anything yet but will be updated in the future
 //
 
 import UIKit
--- HogentProject/QuestionTableViewController.swift
+++ HogentProject/QuestionTableViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Tim Schotte on 29/12/2018.
 //  Copyright © 2018 Tim Schotte. All rights reserved.
+//  (c) I extended some exercises from the book "Everyone can code - App developement with swift" downloaded from the ibooks store
+//  It's possible there are similarities 
 //
 
 import UIKit
